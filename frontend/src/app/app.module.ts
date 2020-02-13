02d51114ca35c038c69348913c34102003be8107
NatholBMX <snake-lizard@freenet.de>
2020-02-13 11:18:21 +0100

    Implement loader and http interceptor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { ReactiveFormsModule } from "@angular/forms";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
@@ -11,6 +11,9 @@ import { TodoListComponent } from "./todo-list/todo-list.component";
 import { TodoItemComponent } from "./todo-item/todo-item.component";
 import { TodoItemService } from "./services/todo-item.service";
 import { AddTodoComponent } from "./add-todo/add-todo.component";
+import { LoaderComponent } from "./shared/loader/loader.component";
+import { LoaderService } from "./services/loader.service";
+import { LoaderInterceptor } from "./shared/interceptors/loader.interceptor";
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { AddTodoComponent } from "./add-todo/add-todo.component";
     CategoryListComponent,
     TodoListComponent,
     TodoItemComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +31,11 @@ import { AddTodoComponent } from "./add-todo/add-todo.component";
     ReactiveFormsModule,
     FontAwesomeModule
   ],
-  providers: [TodoItemService],
+  providers: [
+    TodoItemService,
+    LoaderService,
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
